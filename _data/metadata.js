db3b4562433395fb9480dcbeb6c426f31730ea7c
geomydas <155147963+geomydas@users.noreply.github.com>
2024-11-30 13:36:15 +0800

    initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+  title: "randev",
+  url: "https://geomydas.github.io/randev/",
+  language: "en",
+  description: "A random  blog focusing on web development.",
+  author: {
+    name: "Jiro",
+    email: "8nvgh39z@anonaddy.me",
+    url: "https://geomydas.github.io/randev/",
+  },
+};
